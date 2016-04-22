93db6cf2826afb7b53416a2ae528d3fc76b56d4e
hype77 <bjma15@student.aau.dk>
2016-04-22 22:30:19 +0200

    Added ToolMove

--- A319TS/A319TS/Resources.Designer.cs
+++ A319TS/A319TS/Resources.Designer.cs
@@ -153,6 +153,16 @@ namespace A319TS {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ToolMove {
+            get {
+                object obj = ResourceManager.GetObject("ToolMove", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ToolRemove {
             get {
                 object obj = ResourceManager.GetObject("ToolRemove", resourceCulture);
