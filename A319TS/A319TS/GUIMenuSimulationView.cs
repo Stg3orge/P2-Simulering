944e49e20da9cd0bd781f45707d87ffc1ce993c9
hype77 <bjma15@student.aau.dk>
2016-05-09 13:05:54 +0200

    Tidskompleksitet og SimulationViewNOTFINISH

---
+++ A319TS/A319TS/GUIMenuSimulationView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A319TS
+{
+    class GUIMenuSimulationView : Form
+    {
+        private Button StartStop;
+        private Button Primary;
+        private Button Secondary;
+        private Label TimeLabel;
+        private Label Simulation;
+        private Label SetTimeLabel;
+        private TextBox SetTime;
+        private TextBox GetTime;
+        private ProgressBar TimeProgress;
+        
+    }
+}
